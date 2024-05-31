f116eb47bf3731791ceb1dc3172b879ef938be12
Jonathan Zavala <70041382+Jontzavala@users.noreply.github.com>
2024-05-31 10:13:00 -0500

    Using ejs now

--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,7 @@
-const express = require("express");
 const path = require("path");
+
+const express = require("express");
+
 const rootDir = require("../util/path");
 const adminData = require("./admin");
 
@@ -7,7 +9,14 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
